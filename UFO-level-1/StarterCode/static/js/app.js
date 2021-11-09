a9d1d4e80111de176f4654f7977fe761fe905f97
SerenaZhaoYanqing <87590423+SerenaZhaoYanqing@users.noreply.github.com>
2021-11-09 18:23:08 +1100

    PART 1--Create a table

--- UFO-level-1/StarterCode/static/js/app.js
+++ UFO-level-1/StarterCode/static/js/app.js
@@ -2,3 +2,33 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+
+// Part 1: Using the UFO dataset provided in the form of an array of JavaScript objects
+// Write code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+
+// get a reference to the table body
+var tbody=d3.select ("tbody");
+// loop  though data and console each UFO object
+data.forEach(function(UFODetails){
+    console.log(UFODetails);
+// append table row (tr) for each UFO details
+    var row = tbody.append("tr");
+// use object.entries to console.log each UFO details 
+    Object.entries(UFODetails).forEach(function([key, value]) {
+        console.log(key, value);
+// use D3 to append 1 cell per DFO value( datetime, city, state etc)
+    var cell = row.append("td");
+// use d3 to update each cell's text with UFO details values 
+    cell.text(value);   
+});
+});
+
+// Make sure you have a column for date/time, city, state, country, shape, and comment at the very least.
+
+
+
+
+
+
+// Use a date form in your HTML document and write JavaScript code that will listen for events and 
+//search through the date/time column to find rows that match user input.
